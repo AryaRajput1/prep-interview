c41ced23f18dfb6c71b58b9450a767d196d6d865
Arya Rajput <rajputarya10@gmail.com>
2025-04-08 19:15:13 +0530

    setup interview card

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function RootLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">PrepInterview</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 }
 export default RootLayout;
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,47 @@
+import InterviewCard from "@/components/interviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 function RootPage() {
-    return (
-      <div>RootPage</div>
-    )
-  }
-  export default RootPage
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg text-gray-500">
+            Practice coding interviews with AI-powered feedback.
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href={"/interview"}>Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          aria-hidden
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {/* <p>You haven't taken any interviews yet.</p> */}
+          { dummyInterviews.map(interview => (
+            <InterviewCard {...interview} key={interview.id}/>
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          <p>There are no Interviews available.</p>
+        </div>
+      </section>
+    </>
+  );
+}
+export default RootPage;

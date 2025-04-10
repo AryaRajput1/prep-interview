01c0165e50dac7c5c37b574e95d546d6963a0cfa
Arya Rajput <rajputarya10@gmail.com>
2025-04-10 18:49:46 +0530

    auth error fix

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -2,6 +2,13 @@ import { initializeApp, getApps, cert, App, getApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
+const serviceKeyBase64 = process.env.FIREBASE_SERVICE_KEY;
+if (!serviceKeyBase64) {
+  throw new Error("Missing Firebase service key");
+}
+
+const serviceKey = JSON.parse(Buffer.from(serviceKeyBase64, "base64").toString("utf8"));
+
 // Initialize Firebase Admin SDK
 function initFirebaseAdmin() {
   const apps = getApps();
@@ -9,12 +16,7 @@ function initFirebaseAdmin() {
 
   if (!apps.length) {
     app = initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace newlines in the private key
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
+      credential: cert(serviceKey)
     });
   } else {
     app = getApp()

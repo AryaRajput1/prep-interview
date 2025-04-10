1c95903e9b7faf01ae445b52a323a9252cc41b2c
Arya Rajput <rajputarya10@gmail.com>
2025-04-10 18:42:29 +0530

    fix authentication error

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,13 +1,14 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { initializeApp, getApps, cert, App, getApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 // Initialize Firebase Admin SDK
 function initFirebaseAdmin() {
   const apps = getApps();
+  let app: App;
 
   if (!apps.length) {
-    initializeApp({
+    app = initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -15,11 +16,13 @@ function initFirebaseAdmin() {
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
     });
+  } else {
+    app = getApp()
   }
 
   return {
-    auth: getAuth(),
-    db: getFirestore(),
+    auth: getAuth(app),
+    db: getFirestore(app),
   };
 }
 
